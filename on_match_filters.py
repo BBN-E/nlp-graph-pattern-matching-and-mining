7c6bf7fb4c7751c60aa9bc3e9b9f7b4686ed0c89
Benjamin V Rozonoyer <brozonoy@d712.bbn.com>
2021-12-21 22:03:35 -0500

    rename variabls

--- on_match_filters.py
+++ on_match_filters.py
@@ -1,16 +1,16 @@
 import networkx
 
-def is_ancestor(match_to_pattern, document_graph, ancestor_id='CCOMP_TOKEN', descendant_id='EVENT_TOKEN'):
+def is_ancestor(match_node_id_to_pattern_node_id, document_graph, ancestor_id='CCOMP_TOKEN', descendant_id='EVENT_TOKEN'):
     '''
-    :param match_to_pattern:  {'a1362': 'CONCEIVER_NODE', 'a1378': 'EVENT_NODE', 'policymakers_a95': 'CONCEIVER_TOKEN', 'epidemic_a91': 'EVENT_TOKEN', 'said_a96': 'SIP', 'need_a81': 'CCOMP_TOKEN'}
+    :param match_node_id_to_pattern_node_id:  {'a1362': 'CONCEIVER_NODE', 'a1378': 'EVENT_NODE', 'policymakers_a95': 'CONCEIVER_TOKEN', 'epidemic_a91': 'EVENT_TOKEN', 'said_a96': 'SIP', 'need_a81': 'CCOMP_TOKEN'}
     :param document_graph:  networkx graph
     :param ancestor_id:
     :param descendant_id:
     :return:
     '''
 
-    pattern_to_match = dict(map(reversed, match_to_pattern.items()))
-    ancestor_match_id = pattern_to_match[ancestor_id]
-    descendant_match_id = pattern_to_match[descendant_id]
+    pattern_node_id_to_match_node_id = dict(map(reversed, match_node_id_to_pattern_node_id.items()))
+    ancestor_match_id = pattern_node_id_to_match_node_id[ancestor_id]
+    descendant_match_id = pattern_node_id_to_match_node_id[descendant_id]
 
     return ancestor_match_id in networkx.algorithms.dag.ancestors(document_graph, descendant_match_id)
