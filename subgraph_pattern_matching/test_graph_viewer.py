05b5e990c7717f02718d6125f107574019499429
David Zajic <david.m.zajic@raytheon.com>
2022-01-20 12:29:46 -0500

    Cleans up test script for GraphViewer

--- subgraph_pattern_matching/test_graph_viewer.py
+++ subgraph_pattern_matching/test_graph_viewer.py
@@ -11,45 +11,10 @@ from constants import NodeTypes, EdgeTypes, \
 
 from graph_builder import GraphBuilder
 from graph_viewer import GraphViewer
-from digraph_matcher_factory import DiGraphMatcherFactory
-from match_wrapper import MatchWrapper, MatchCorpus
 
 def graph_view(serif_doc, workspace):
     GB = GraphBuilder()
     GV = GraphViewer()
-    # document_graph = GB.convert_serif_doc_to_networkx(serif_doc)
-    # document_graph_html = os.path.join(workspace,"document_graph.html")
-    # GB.visualize_networkx_graph(document_graph, document_graph_html)
-
-    # MDPG = GB.modal_dependency_parse_to_networkx(serif_doc)
-    # SDPG = [GB.syntactic_dependency_parse_to_networkx(s) for s in serif_doc.sentences]
-    # mdpg_html = os.path.join(workspace,"mdpg_graph.html")
-    # GB.visualize_networkx_graph(MDPG, mdpg_html)
-    # for i, sdpg in enumerate(SDPG):
-    #     sdpg_html = os.path.join(workspace,"sdpg_{:02d}_graph.html".format(i))
-    #     GB.visualize_networkx_graph(sdpg, sdpg_html)
-        
-    # H = GB.serif_doc_to_mdp_graph(serif_doc)
-    # GB.visualize_networkx_graph(H, os.path.join(workspace,"mpdg_graph.html"))
-
-    # graphs = GB.serif_doc_to_mdp_graphs_by_sentence(serif_doc)
-    # for i, H in enumerate(graphs):
-    #     print("len of H is {}".format(len(H)))
-    #     GB.visualize_networkx_graph(H, os.path.join(workspace, "mdpg_{:02d}_graph.html".format(i)))
-
-    # G = GB.serif_doc_to_mdp_graph(serif_doc)
-    # GB.visualize_networkx_graph(G, os.path.join(workspace, "mdpg_graph.html"))
-
-    # GB.initialize_token_to_mdp_node(serif_doc)
-    # for i, sentence in enumerate(serif_doc.sentences):
-    #     H = GB.syntactic_dependency_parse_to_networkx(sentence, root_depth=3)
-    #     I = GB.mdp_parse_for_sentence_to_networkx(sentence, root_depth=3)
-    #     GB.visualize_networkx_graph(H, os.path.join(workspace,"sdpg_{:02d}_graph.html".format(i)))
-    #     GB.visualize_networkx_graph(I, os.path.join(workspace,"mdpg_{:02d}_graph.html".format(i)))
-    #     # note: arg order matters. node levels are preserved from 2nd arg
-    #     J = nx.algorithms.operators.compose(I,H)
-    #     GB.visualize_networkx_graph(J, os.path.join(workspace,"compose_{:02d}_graph.html".format(i)))
-    
 
     G = GB.modal_dependency_parse_to_networkx(serif_doc)
     GV.prepare_mdp_networkx_for_visualization(G)
@@ -64,14 +29,6 @@ def graph_view(serif_doc, workspace):
         J = nx.algorithms.operators.compose(F,H)
         GV.visualize_networkx_graph(J, os.path.join(workspace,"compose_{:02d}_graph.html".format(i)))
 
-    for node in G:
-        in_edges = len(G.in_edges(node))
-        if in_edges == 0:
-            print("{} {} {} {}".format(
-                node, 
-                G.in_edges(node),
-                G[node],
-                G.nodes[node]))
 
 def main(args):
     if args.list:
