c1c4c6c22c3fb8ffa4a6513c6668d7e2b4d28bec
David Zajic <david.m.zajic@raytheon.com>
2022-01-21 14:14:49 -0500

    Updates test scripts for graph visualization

--- subgraph_pattern_matching/test_graph_viewer.sh
+++ subgraph_pattern_matching/test_graph_viewer.sh
@@ -15,8 +15,13 @@ SERIFXML=/nfs/raid66/u11/users/brozonoy-ad/subgraph-pattern-matching/mds_appendi
 # SERIFXML=/nfs/mercury-13/u124/dzajic/aida/mtdp/workspace/L0C04958H.rsd.xml
 # SERIFXML=/nfs/mercury-13/u124/dzajic/aida/mtdp/workspace/L0C04958I.rsd.xml
 # SERIFXML=/nfs/mercury-13/u124/dzajic/aida/mtdp/output/mtdp.LDC2021E11.2022-01-14.nlplingo.small/L0C049DQH.mt.xml
-SERIFXML=/nfs/mercury-13/u124/dzajic/aida/mtdp/output/mtdp.LDC2021E11.20220117/L0C04958D.rsd.xml
-WORKSPACE=/nfs/raid91/u10/developers/dzajic-ad/projects/rozonoyer/subgraph-pattern-matching/workspace/html
+# SERIFXML=/nfs/mercury-13/u124/dzajic/aida/mtdp/output/mtdp.LDC2021E11.20220117/L0C04958D.rsd.xml
+# WORKSPACE=/nfs/raid91/u10/developers/dzajic-ad/projects/rozonoyer/subgraph-pattern-matching/workspace/html
+
+DOC=L0C04958I
+SERIFXML=/nfs/mercury-13/u124/dzajic/aida/mtdp/output/mtdp.LDC2021E11.20220117/$DOC.rsd.xml
+WORKSPACE=/nfs/raid91/u10/developers/dzajic-ad/projects/rozonoyer/subgraph-pattern-matching/workspace/html.$DOC
 
 # PYTHONPATH=$TEXTOPEN python $SGPM/extract_claims.py -i $SERIFXML -v
+mkdir -p $WORKSPACE
 PYTHONPATH=$TEXTOPEN python $SGPM/test_graph_viewer.py -i $SERIFXML -w $WORKSPACE
