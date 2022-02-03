7880e88e9c8ed7bdbc8907849a0753e3348ac572
Benjamin V Rozonoyer <brozonoy@d712.bbn.com>
2022-02-03 11:37:27 -0500

    modularize

--- subgraph_pattern_matching/match_utils/node_match_functions.py
+++ subgraph_pattern_matching/match_utils/node_match_functions.py
@@ -2,34 +2,74 @@ from constants import *
 
 
 def node_type_match(n1, n2):
+    '''
+    :param n1: document graph node, e.g. {'id': 'based__a118', 'nodeType': 'token', 'text': 'based', 'upos': 'VERB', 'xpos': 'VBN', 'indexInDoc': '2_3_3', 'incomingDepRel': 'amod'}
+    :param n2: pattern graph node, e.g. {'nodeType': 'modal', 'modalNodeType': 'Conceiver', 'specialName': 'AUTHOR_NODE'}
+    :return: boolean indicating whether the general node types match
+    '''
+
     return n1[NodeAttrs.node_type] == n2[NodeAttrs.node_type]  # modal, token
 
-def node_modal_type_match(n1, n2):
+
+def node_attr_match(n1, n2, attr):
+    '''
+    :param n1: document graph node, e.g. {'id': 'based__a118', 'nodeType': 'token', 'text': 'based', 'upos': 'VERB', 'xpos': 'VBN', 'indexInDoc': '2_3_3', 'incomingDepRel': 'amod'}
+    :param n2: pattern graph node, e.g. {'nodeType': 'modal', 'modalNodeType': 'Conceiver', 'specialName': 'AUTHOR_NODE'}
+    :param attr: node attribute used to match n1 and n2
+    :return: boolean indicating whether the nodes match w.r.t. attr
+    '''
+
     if not node_type_match(n1, n2):
         return False
-    if ((ModalNodeAttrs.modal_node_type in n1) and (ModalNodeAttrs.modal_node_type not in n2)) or \
-       ((ModalNodeAttrs.modal_node_type in n2) and (ModalNodeAttrs.modal_node_type not in n1)):
-        return True  # if one of the nodes is underspecified in modal node type, then the modal node types match
-    return n1.get(ModalNodeAttrs.modal_node_type, None) == n2.get(ModalNodeAttrs.modal_node_type, None)  # Conceiver, Event
+    if ((attr in n1) and (attr not in n2)) or \
+       ((attr in n2) and (attr not in n1)):
+        return True  # if one of the nodes is underspecified w.r.t. attr, then the attrs match
+    return n1.get(attr, None) == n2.get(attr, None)
+    # TODO permit pattern node n2 to specify conjunction of attrs, e.g. "VERB|ADJ"
+    #    return n1.get(TokenNodeAttrs.upos, "ε") in set(n2.get(TokenNodeAttrs.upos, "ε").split("|"))
+
+def node_multiple_attrs_match(*match_fns):
+
+    def node_multiple_attrs_match_fn(n1, n2):
+        return all(match_fn(n1, n2) for match_fn in match_fns)
+
+    return node_multiple_attrs_match_fn
+
+#######################################################
+#######   SINGLE ATTR NODE MATCHING FUNCTIONS   #######
+#######################################################
+
+def node_modal_type_match(n1, n2):
+    return node_attr_match(n1, n2, attr=ModalNodeAttrs.modal_node_type)
+
+def node_special_name_match(n1, n2):
+    if not node_modal_type_match(n1, n2):
+        return False
+    return node_attr_match(n1, n2, attr=ModalNodeAttrs.special_name)
 
 def node_upos_match(n1, n2):
-    if not node_type_match(n1, n2):
-        return False
-    if ((TokenNodeAttrs.upos in n1) and (TokenNodeAttrs.upos not in n2)) or \
-       ((TokenNodeAttrs.upos in n2) and (TokenNodeAttrs.upos not in n1)):
-        return True  # if one of the nodes is underspecified in pos, then the pos match
-    return n1.get(TokenNodeAttrs.upos, None) == n2.get(TokenNodeAttrs.upos, None)  # VERB, NOUN, ADJ etc.
+    return node_attr_match(n1, n2, attr=TokenNodeAttrs.upos)
+
+def node_incoming_dep_rel_match(n1, n2):
+    return node_attr_match(n1, n2, attr=TokenNodeAttrs.incoming_dep_rel)
 
 def node_text_match(n1, n2):
-    if not node_upos_match(n1, n2):
-        return False
-    if ((TokenNodeAttrs.text in n1) and (TokenNodeAttrs.text not in n2)) or \
-       ((TokenNodeAttrs.text in n2) and (TokenNodeAttrs.text not in n1)):
-        return True  # if one of the nodes is underspecified in text, then the text match
-    return n1.get(TokenNodeAttrs.text, None) == n2.get(TokenNodeAttrs.text, None)
+    return node_attr_match(n1, n2, attr=TokenNodeAttrs.text)
 
-def node_modal_type_and_upos_match(n1, n2):
-    return node_modal_type_match(n1, n2) and node_upos_match(n1, n2)
 
-def node_modal_type_and_text_match(n1, n2):
-    return node_modal_type_match(n1, n2) and node_text_match(n1, n2)
+# TODO these are obsolete with the implementation of node_multiple_attrs_match(*match_fns) above
+# #########################################################
+# #######   MULTIPLE ATTR NODE MATCHING FUNCTIONS   #######
+# #########################################################
+#
+# def node_modal_type_and_upos_match(n1, n2):
+#     return node_modal_type_match(n1, n2) and node_upos_match(n1, n2)
+#
+# def node_modal_type_and_upos_and_incoming_dep_rel_match(n1, n2):
+#     return node_modal_type_match(n1, n2) and node_upos_match(n1, n2) and node_incoming_dep_rel_match(n1, n2)
+#
+# def node_modal_type_and_text_match(n1, n2):
+#     return node_modal_type_match(n1, n2) and node_text_match(n1, n2)
+#
+# def node_modal_type_and_special_name_and_upos_and_incoming_dep_rel_match(n1, n2):
+#     return node_modal_type_match(n1, n2) and node_special_name_match(n1, n2) and node_upos_match(n1, n2) and node_incoming_dep_rel_match(n1, n2)
